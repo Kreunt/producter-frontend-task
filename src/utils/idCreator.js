69c938220a8fcc05131f1b0881ec1e55e21f1b45
Kreunt <74057348+Kreunt@users.noreply.github.com>
2022-10-27 01:15:34 +0300

    change deprecated substr func to slice

--- src/utils/idCreator.js
+++ src/utils/idCreator.js
@@ -1,4 +1,4 @@
 const idCreator = () => {
-  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+  return Date.now().toString(36) + Math.random().toString(36).slice(2);
 };
 export default idCreator;
