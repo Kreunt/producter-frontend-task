3d62581b8e58bece0f80c25c8df205358537c2dc
Kreunt <74057348+Kreunt@users.noreply.github.com>
2022-10-27 01:57:43 +0300

    add handleClear func to List

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,27 +1,29 @@
 import ListRow from "./list-row.jsx";
 import "../css/list.css";
 
-const List = ({ list, setList }) => (
-  <ul>
-    {list.length > 0 ? (
-      list.map((item) => (
-        <ListRow key={item.id} item={item} setList={setList} />
-      ))
-    ) : (
-      <p>No items</p>
-    )}
-    <button
-      onClick={() => {
-        setList((prevList) => {
-          return prevList.filter((item) => {
-            return !item.completed;
-          });
-        });
-      }}
-    >
-      Clear Completed
-    </button>
-  </ul>
-);
+const List = ({ list, setList }) => {
+  const handleClear = () => {
+    setList((prevList) => {
+      return prevList.filter((item) => {
+        return !item.completed;
+      });
+    });
+  };
+
+  return (
+    <ul className="table">
+      {list.length > 0 ? (
+        list.map((item) => (
+          <ListRow key={item.id} item={item} setList={setList} />
+        ))
+      ) : (
+        <p>No items</p>
+      )}
+      <button className="clear-button" onClick={handleClear}>
+        Clear Completed
+      </button>
+    </ul>
+  );
+};
 
 export default List;
